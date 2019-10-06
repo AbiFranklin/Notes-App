a4a06e760270b90e4d2849779757ffdebd7d3b08
Abigayle Franklin <abifanklin@gmail.com>
2019-10-06 10:37:15 -0500

    Morgan added for logging of server requests

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 //Load server
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('short'))
 
 app.get("/", (req, res) => {
     console.log("Responding to root route")
     res.send("Hello from the root")
+})
+
+app.get("/users", (req, res) => {
+    let user = {id: 1, username: 'Abi'}
+    res.json(user)
+})
+
+app.get("/posts", (req, res) => {
+    console.log("Responding to posts route")
+    res.send("List of posts")
 })
 
 //localhost:3003
