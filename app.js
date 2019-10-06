e3a56fe1f39e3e01c1bc56ba70597cf9724962ad
Abigayle Franklin <abifanklin@gmail.com>
2019-10-06 11:01:25 -0500

    GET req from deployed Db working

--- app.js
+++ app.js
@@ -2,22 +2,41 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const mysql = require('mysql')
+require('dotenv').config()
 
 app.use(morgan('short'))
 
+//Connect to database
+const connection = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.ADMINUSER,
+    password: process.env.PASSWORD,
+    port: process.env.PORT,
+    database: 'franklin_test'
+})
+
+//Route handling
 app.get("/", (req, res) => {
     console.log("Responding to root route")
     res.send("Hello from the root")
 })
 
 app.get("/users", (req, res) => {
-    let user = {id: 1, username: 'Abi'}
-    res.json(user)
+    connection.query("SELECT * FROM user", (err, rows, fields) => {
+        res.json(rows)
+    })
+})
+
+app.get("/users/:id", (req, res) => {
+    const connection = 
+    res.json(req.params.id)
 })
 
 app.get("/posts", (req, res) => {
-    console.log("Responding to posts route")
-    res.send("List of posts")
+    connection.query("SELECT * FROM posts", (err, rows, fields) => {
+        res.json(rows)
+    })
 })
 
 //localhost:3003
