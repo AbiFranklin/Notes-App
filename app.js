9e46e4b29e0d4033738bb74eb69d3cfc6bba10d2
Abigayle Franklin <Abifranklin@gmail.com>
2019-10-17 20:42:27 -0500

    Update app.js

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.get("/users/:id", (req, res) => {
 
 app.delete("/posts/:id", (req, res) => {
     connection.query(`DELETE * FROM posts WHERE id=${req.params.id}`, (err, rows, fields) => {
-        res.json(rows)
+        res.json(req.params.id)
         if (err) {
             console.log(err)
         }
